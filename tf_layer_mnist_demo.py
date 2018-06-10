de0e301939cd43462bf4b1d2e084d342af914ae7
DzianisH <gavrilovetsden@gmail.com>
2018-06-10 16:18:06 +0300

    Improve TF Layer API MNIST demo model

--- tf_layer_mnist_demo.py
+++ tf_layer_mnist_demo.py
@@ -11,9 +11,10 @@ img_size_flat = img_size * img_size
 img_shape = (img_size, img_size)
 num_channels = 1
 num_classes = 10
-epochs = 12
+max_epochs = 101
 train_batch_size = 100
 test_batch_size = 500
+max_epoch_without_improvement = 25
 
 # utils.plot_images(data.test.images[:9], img_shape, data.test.labels[:9])
 
@@ -38,6 +39,7 @@ with utils.create_session() as sess:
 model = layers.conv2d(inputs=model, name="conv1", padding='same', filters=32, kernel_size=3, activation=tf.nn.relu)
 model = layers.conv2d(inputs=model, name="conv2", padding='same', filters=64, kernel_size=3, activation=tf.nn.relu)
 model = layers.max_pooling2d(inputs=model, name="pool1", strides=2, pool_size=2)
+model = layers.conv2d(inputs=model, name="conv3", padding='same', filters=64, kernel_size=3, activation=tf.nn.relu)
 model = layers.dropout(inputs=model, rate=0.25)
 model = layers.flatten(model)
 model = layers.dense(inputs=model, name='dense1', units=128, activation=tf.nn.relu)
@@ -54,16 +56,18 @@ guesses = tf.reduce_sum(guesses)
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=logits)
 loss = tf.reduce_mean(cross_entropy)
 
-optimazer = tf.train.MomentumOptimizer(learning_rate=3e-2, momentum=0.9).minimize(loss)
+optimazer = tf.train.MomentumOptimizer(learning_rate=3e-3, momentum=0.9).minimize(loss)
 # merged_summary_op = tf.summary.merge_all()
 # weights_conv1 = utils.get_weight_variable('conv1')
 # weights_conv2 = utils.get_weight_variable('conv2')
 
 with utils.create_session() as sess:
+    saver = tf.train.Saver()
     sess.run(tf.global_variables_initializer())
 
     best_test_accuracy = -1
-    for epoch in range(epochs):
+    best_test_accuracy_epoch = 0
+    for epoch in range(max_epochs):
         train_accuracy, train_loss, train_time_spent = utils.train(data.train, train_batch_size,
                                                                    lambda batch_x, batch_y: sess.run(
                                                                        [optimazer, guesses, loss],
@@ -80,4 +84,14 @@ with utils.create_session() as sess:
                                                                                              test_time_spent))
         if best_test_accuracy < test_accuracy:
             best_test_accuracy = test_accuracy
+            best_test_accuracy_epoch = epoch
+            print("           new best accuracy")
+            saver.save(sess, "data/models/mnist_{:.3f}".format(test_accuracy), latest_filename="best")
+
+        if best_test_accuracy_epoch + max_epoch_without_improvement < epoch:
+            print("\texceeded maximum amount of epoch without score improvement (max_dist: {}, current_epoch: {}, "
+                  "best_epoch: {})".format(max_epoch_without_improvement, epoch, best_test_accuracy_epoch))
+            break
+
+    saver.save(sess, "data/modes/mnist_{:.3f}".format(best_test_accuracy), latest_filename="last")
     print("best_test_accuracy: {:.2f}%".format(best_test_accuracy))
